a9411d8bd4d7ff12e458878c9d7c58c4d161db3d
Mihnea Seitoaru <mihnea.seitoaru@msg.group>
2024-07-31 10:17:46 +0300

    Added zod validation

--- src/components/products/edit-product-form/edit-product-form.tsx
+++ src/components/products/edit-product-form/edit-product-form.tsx
@@ -1,8 +1,21 @@
 import { SubmitHandler, useForm } from "react-hook-form";
-import { EditProductDTO } from "../../../interfaces/products/product.interface";
 import "./edit-product-form.scss";
 import productService from "../../../services/products/product.service";
 import { useParams } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const Schema = z.object({
+  name: z.string().min(2),
+  category: z.string(),
+  price: z.coerce.number(),
+  weight: z.string().transform((value) => parseInt(value)),
+  supplier: z.string(),
+  imageUrl: z.string(),
+  description: z.string(),
+});
+
+type FormFields = z.infer<typeof Schema>;
 
 export default function EditProductForm() {
   const { id } = useParams();
@@ -10,92 +23,63 @@ export default function EditProductForm() {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors, isSubmitting },
-  } = useForm<EditProductDTO>();
+  } = useForm<FormFields>({
+    defaultValues: {
+      name: "", //add all the fields and use the values from the edited file
+    },
+    resolver: zodResolver(Schema),
+  });
 
-  const isDecimal = (value: string) => /^\d+(\.\d+)?$/.test(value);
-
-  const onSubmit: SubmitHandler<EditProductDTO> = async (data) => {
-    await productService.update(id!, data);
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await productService.update(id!, data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError("root", {
+          message: err.message,
+        });
+      }
+    }
   };
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register("name", {
-            required: "Name is required",
-          })}
-          type="text"
-          placeholder="Name"
-        />
+        <input {...register("name")} type="text" placeholder="Name" />
         {errors.name && <div className="text-red">{errors.name.message}</div>}
         <select {...register("category")}>
           <option value="1">Smartphones</option>
         </select>
-        <input
-          {...register("price", {
-            required: "Price is required",
-            validate: (value) => {
-              if (!isDecimal(value.toString())) {
-                return "Price must be a decimal";
-              }
-              return true;
-            },
-          })}
-          type="text"
-          placeholder="Price"
-        />
+        <input {...register("price")} type="number" placeholder="Price" />
         {errors.price && <div className="text-red">{errors.price.message}</div>}
-        <input
-          {...register("weight", {
-            required: "Weight is required",
-            validate: (value) => {
-              if (!isDecimal(value.toString())) {
-                return "Weight must be a decimal";
-              }
-              return true;
-            },
-          })}
-          type="text"
-          placeholder="Weight"
-        />
+        <input {...register("weight")} type="number" placeholder="Weight" />
         {errors.weight && (
           <div className="text-red">{errors.weight.message}</div>
         )}
-        <input
-          {...register("supplier", {
-            required: "Supplier is required",
-          })}
-          type="text"
-          placeholder="Supplier"
-        />
+        <input {...register("supplier")} type="text" placeholder="Supplier" />
         {errors.supplier && (
           <div className="text-red">{errors.supplier.message}</div>
         )}
-        <input
-          {...register("imageUrl", {
-            required: "Image URL is required",
-          })}
-          type="text"
-          placeholder="Image URL"
-        />
+        <input {...register("imageUrl")} type="text" placeholder="Image URL" />
         {errors.imageUrl && (
           <div className="text-red">{errors.imageUrl.message}</div>
         )}
         <input
-          {...register("description", {
-            required: "Description is required",
-          })}
+          {...register("description")}
           type="textarea"
           placeholder="Description"
         />
         {errors.description && (
           <div className="text-red">{errors.description.message}</div>
         )}
+
         <button disabled={isSubmitting} type="submit">
           {isSubmitting ? "Loading..." : "Submit"}
         </button>
+
+        {errors.root && <div className="text-red">{errors.root.message}</div>}
       </form>
     </>
   );
